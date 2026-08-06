3d48cb5ffb4c0be1a5cfabbf686a593f6eba218a
jeremyrc760 <jeremyin760@outlook.com>
2026-08-06 12:54:11 -0400

    WIP

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -7,11 +7,13 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 
 const app = express();
 app.use("*",cors());
 app.use(express.json());
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
+app.use('api/secondchance/search', searchRoutes);
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
