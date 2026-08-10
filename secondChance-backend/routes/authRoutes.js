9447890c6532367d52bd4f1ed6f9003863bc4a28
jeremy <jeremyin760@outlook.com>
2026-08-10 13:03:57 -0400

    added user login function

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -55,4 +55,47 @@ router.post('/register', async (req, res) => {
         }
 });
 
+router.post('/login', async(req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection = await db.collection('users');
+		// Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({email: req.body.email});
+		// Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if (!result) {
+                logger.error('Password do not match');
+                return res.status(404).json({error: 'Wrong password'});
+            }
+        
+        // Task 5: Fetch user details from a database
+        let payload = {
+            user: {
+                id: theUser._id.toString(),
+            },
+        };
+
+        const userName = theUser.firstName;
+        const userEmail = theUser.email;
+
+		// Task 6: Create JWT authentication if passwords match with user._id as payload
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User logged in successfully');
+        return res.status(200).json({authtoken, userName, userEmail});
+
+		// Task 7: Send appropriate message if the user is not found
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({error: 'User not found'});
+        }
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).json({error: 'Internal server error', details: e.message});
+
+    }
+});
+
 module.exports = router;
