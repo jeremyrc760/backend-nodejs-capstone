42d76e17ba72e1aa24db4a1571ddee677e34183d
jeremy <jeremyin760@outlook.com>
2026-08-10 21:01:14 -0400

    run eslint for all files

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -52,8 +52,8 @@ router.post('/', upload.single('file'), async (req, res, next) => {
       secondChanceItem.id = (parseInt(item.id) + 1).toString()
     })
     // Step 3: task 5 - insert code here
-    const date_added = Math.floor(new Date().getTime() / 1000)
-    secondChanceItem.date_added = date_added
+    const dateAdded = Math.floor(new Date().getTime() / 1000)
+    secondChanceItem.dateAdded = dateAdded
 
     secondChanceItem = await collection.insertOne(secondChanceItem)
     console.log(secondChanceItem)
